b27abb6b934a533e35a5f52d75d8d069967ffa6c
Studentblanchard <Studentblanchard@users.noreply.github.com>
2016-04-12 20:53:38 -0500

    update file system

--- mkfs.c
+++ mkfs.c
@@ -24,7 +24,9 @@ int nbitmap = FSSIZE/(BSIZE*8) + 1;
 int ninodeblocks = NINODES / IPB + 1;
 int nlog = LOGSIZE;  
 int nmeta;    // Number of meta blocks (boot, sb, nlog, inode, bitmap)
-int nblocks;  // Number of data blocks
+int nblocks = 202985;
+int ninodes = 200;
+int size = 21029;
 
 int fsfd;
 struct superblock sb;
